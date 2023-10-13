32f4ddd8c71673f9ce843163d0ec93a81dc9412e
Ravish-Ranjan <122588009+Ravish-Ranjan@users.noreply.github.com>
2023-10-13 10:37:12 +0530

    fetching media

--- project/fetchmedia.js
+++ project/fetchmedia.js
@@ -14,7 +14,7 @@ function getkey() {
 async function fetchInfo(moviename) {
     const res = await fetch(`http://www.omdbapi.com/?apikey=${getkey().toString()}&t=${moviename}`);
     const md = await res.json();
-    return md
+    return md;
 }
 
 function scandir(){
@@ -23,24 +23,41 @@ function scandir(){
     return authen.readFiles(scanfile);
 }
 
-function mediaLists() {
-    let res = scandir()["folders"];
-    let ret = {};
-    for (let i = 0; i < res.length; i++) {
-        let medlist = [];
-        let subfold = res[i]["folders"];
-        for (let j = 0; j < subfold.length; j++) {
-            medlist.push(subfold[j]["name"]);
+
+function isSame(arr1,arr2) {
+    if (arr1.lenght !== arr2.lenght) {
+        return false;
+    } else {
+        let shorter = (arr1.length > arr2.lenght) ? arr2 : arr1;
+        let longer = (shorter === arr1) ? arr2 : arr1;
+        for (let i = 0; i < shorter.length; i++) {
+            if (longer.includes(shorter[i])) {
+                let torem = shorter[i]
+                longer = longer.filter(elem => elem !== torem);
+                shorter = shorter.filter(elem => elem !== torem);
+                if (shorter.lenght === longer.lenght) {
+                    continue;
+                }
+                return false;
+            } else {
+                return false;
+            }
         }
-        ret[res[i]["name"]] = medlist;
+        return true;
     }
-    return ret;
 }
+
+function getMeta() {
+    let metas = fs.readdirSync(mediafold);
+    console.log(metas);
+}
+
+// getMeta();
 
 module.exports = {
     scandir:scandir,
-    mediaLists:mediaLists,
-    fetchInfo,fetchInfo
+    fetchInfo,fetchInfo,
+    isSame,isSame
 };
 
 // fetchInfo("The Dictator").then(data => {console.log(data);});
--- project/index.js
+++ project/index.js
@@ -13,6 +13,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.set("view engine","ejs")
 
+
 app.get("/",(req,res) => {
     res.render(__dirname + "/views/sisu.ejs");
 });
@@ -22,7 +23,7 @@ app.post("/signin",(req,res) => {
     let result = authen.searchUser(form.username);
     if (result) {
         if (result[2] === form.password) {
-            res.render("homepage.ejs",{name:result[0],media:fetchmed.mediaLists()})
+            res.render("homepage.ejs",{name:result[0]})
         }
         else{
             res.render("logerr.ejs",{err:"Password or Username doesn't match"});
@@ -54,6 +55,36 @@ app.post("/signup",(req,res) => {
             res.render(__dirname + "logerr",{err:"Error While adding user"});
         }
     }
+});
+
+app.get("/admin@3567",(req,res) => {
+    res.render("addmedia.ejs");
+});
+
+app.post("/addmedia",(req,res) => {
+    let form = req.body;
+    let scan = fetchmed.scandir();
+    let addFold;
+    for (const type of scan["folders"]) {
+        if (type["name"] === form["media-type"]) {
+            for (const fold of type["folders"]) {
+                if (fetchmed.isSame(form["media-fold"],fold["files"]) && (!(fold["files"].includes("flkmeta.json")))) {
+                    addFold = fold["root"];
+                }
+                else{
+                    res.render("logerr.ejs",{err:"Media already exists"});
+                }
+            }
+        }
+    }
+
+    let medInfo ;
+    fetchmed.fetchInfo(form["media-name"]).then(data => {
+        data["saved-media-path"] = addFold + form["media-file"];
+        authen.writeFiles(__dirname + addFold + "flkmeta.json",data);
+        res.render("logerr.ejs",{err:"Media added"});
+    });
+    
 });
 
 app.listen(port,() => {
